c76c1872969b885c41f141b4325d5606173a9267
sakib <sbsakibsarkar8@gmail.com>
2024-11-22 01:30:25 +0600

    start project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Btn from './compronent/Btn'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className=' text-red-600'>sb sakib sarkar</h1>
+    <Btn/>
+    </>
+  )
+}
+
+export default App
---
+++ src/compronent/Btn.jsx
@@ -0,0 +1,10 @@
+import {Button} from "@nextui-org/react";
+
+ function Btn() {
+  return (
+    <Button color="primary">
+      Button
+    </Button>
+  );
+}
+export default Btn
