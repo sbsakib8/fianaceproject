2e2a30900f9ceaded83f059a787007b5e5278d6d
sakib <sbsakibsarkar8@gmail.com>
2024-11-22 03:20:27 +0600

    data offer section

--- src/compronent/button/Btn.jsx
+++ src/compronent/button/Btn.jsx
@@ -4,7 +4,8 @@ import {Button, cn} from "@nextui-org/react";
   return (
 
      <Button color="primary" variant="shadow" className={cn(' bg-primary w-[195px] h-[56px] rounded-full text-white ',className)}>
-        {title}
+        {title }
+        {icon}
       </Button>  
    
   );
---
+++ src/data/offer/Offer.js
@@ -0,0 +1,23 @@
+const offer = [
+    {
+        id: 1,
+        img: 'security.png',
+        title:'Cardano Debit-Cards',
+        description: 'Cardano, simplified. The debit card that makes crypto spending easy.',
+        
+    },
+    {
+        id: 2,
+        img: 'Group 23.png',
+        title:'On & Off Ramp',
+        description: 'Fiat meets crypto, effortlessly buy, sell, and manage.',
+        
+    },
+    {
+        id: 3,
+        img: 'analytics.png',
+        title:'Solutions for business',
+        description: 'Empowering businesses with effortless crypto payments and seamless fiat solutions',
+        
+    },
+];
