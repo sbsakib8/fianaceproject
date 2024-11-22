5f5117a366eb471914bd5a444230bee4eb9efae1
sakib <sbsakibsarkar8@gmail.com>
2024-11-23 01:50:28 +0600

    footerdata

---
+++ src/data/footer/Footerdata.js
@@ -0,0 +1,18 @@
+const Footerdat = [
+    {
+        title: 'About Us',
+        links: ['Investors', 'Features', 'Book a demo','Security ']
+    },
+    {
+        title: 'Products',
+        links: ['Credits Cards', 'Gift Cards', 'Savings accounts','NFT']
+    },
+    {
+        title: 'Useful Links',
+        links: [' PoFree rewards', 'Documentation', 'Affiliate program']
+    },
+    {
+        title: 'Social',
+        links: [' Changelog', 'License', 'Site Maps','News']
+    }
+]
