ed44ce96825a352d6f2c442523a3d2226e4e3e21
sakib <sbsakibsarkar8@gmail.com>
2024-11-23 01:41:08 +0600

    reviewdata

--- src/data/offer/Offer.js
+++ src/data/offer/Offer.js
@@ -1,23 +1,27 @@
+import analytics from './analytics';
+import group from './group';
+import security from './security';
 const offer = [
     {
         id: 1,
-        img: 'security.png',
+        img: analytics,
         title:'Cardano Debit-Cards',
         description: 'Cardano, simplified. The debit card that makes crypto spending easy.',
         
     },
     {
         id: 2,
-        img: 'Group 23.png',
+        img: group,
         title:'On & Off Ramp',
         description: 'Fiat meets crypto, effortlessly buy, sell, and manage.',
         
     },
     {
         id: 3,
-        img: 'analytics.png',
+        img: security,
         title:'Solutions for business',
         description: 'Empowering businesses with effortless crypto payments and seamless fiat solutions',
         
     },
 ];
+export default offer;
---
+++ src/data/reviewdata/Reviewdata.js
@@ -0,0 +1,24 @@
+const Reviewdata = [
+    {
+        id: 1,
+        img: 'Ellipse.png',
+        name: 'Hadid Khan',
+        title: 'UIUX Designer',
+        content: 'Great session! Dani was super helpful. She shared some practical advice on how can lorem ip we go about refining our service offerings.',
+    },
+    {
+        id: 2,
+        img: 'Ellipse (1).png',
+        name: 'Wade Warren',
+        title: 'Web Designer',
+        content: "It's is both attractive and highly adaptable. It's exactly what I've been looking forefinitely wo lorem ipsum dolorth the investment.",
+    },
+    {
+        id: 3,
+        img: 'Ellipse (2).png',
+        name: 'Jenny Wilson',
+        title: 'Trust Administrator',
+        content: "I am really satisfied with it. I'm good to go. It really saves me time and effort. It's is exactly what our business has been lacking. ",
+    },
+   
+]
