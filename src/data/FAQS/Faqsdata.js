8f8d23a80f81846af2ff544d3abef9161ebf3eb2
sakib <sbsakibsarkar8@gmail.com>
2024-11-23 01:55:59 +0600

    FAQSDATA

---
+++ src/data/FAQS/Faqsdata.js
@@ -0,0 +1,30 @@
+const Faqsdata=[
+    {
+        question: "What credit score do I need to apply for a credit card?",
+        answer: "The required credit score may vary depending on the specific credit card. Generally, a good to excellent credit score (typically 670 or above) increases your chances of approval for premium credit cards."
+    },
+    {
+        question: " How can I apply for a credit card online?",
+        answer: "We offer free shipping for orders over $50. You can also choose to pay in installments if you prefer. Please note that some shipping options may require additional fees."
+    },
+    {
+        question: "Are there any annual fees associated with the credit card??",
+        answer: "Yes, you can return your product to us within 14 days of receiving it. Please provide the original packaging and return the item to our return address. We will accept cash, credit card, or check as payment."
+    },
+    {
+        question: "How long does it take to receive the credit card once approved?",
+        answer: "Yes, you can return your product to us within 14 days of receiving it. Please provide the original packaging and return the item to our return address. We will accept cash, credit card, or check as payment."
+    },
+    {
+        question: " How can I check my credit card balance and transactions?",
+        answer: "Yes, you can return your product to us within 14 days of receiving it. Please provide the original packaging and return the item to our return address. We will accept cash, credit card, or check as payment."
+    },
+    {
+        question: " What should I do if my credit card is lost or stolen?",
+        answer: "Yes, you can return your product to us within 14 days of receiving it. Please provide the original packaging and return the item to our return address. We will accept cash, credit card, or check as payment."
+    },
+    {
+        question: "Is my credit card information secure?",
+        answer: "Yes, you can return your product to us within 14 days of receiving it. Please provide the original packaging and return the item to our return address. We will accept cash, credit card, or check as payment."
+    },
+]
